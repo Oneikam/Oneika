6584a7d481ee35c4fbcdee14eddf686a9dec6a34
Oneikam <oneikamorgan2@gmail.com>
2025-03-18 11:21:39 -0500

    add file

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hello");
+console.log(5+6);
+console.log(11/2);
+console.log(29+3.5);
+console.log(2025-1969);
+console.log ('Oneika')
+console.log(Math.random()); //create random numbers between 0 and 1
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.floor(Math.random())); //round to the nearest ten
+console.log(Math.floor(Math.random()*50));//round to the nearest ten
+
+var myName ='Oneika';
+console.log(myName); //output Oneika
+
+var favoriteFood = "pizza";
+var numOfSlices = 8;
+console.log(favoriteFood,numOfSlices);
+
+let meal='curry';
+console.log(meal);//value is constant 
+meal='pizza'; //value can be changed
+console.log(meal);
+
